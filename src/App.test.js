a30085f6657f6e9304b8ecdb5c31a1ac1fd55e51
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-02-20 11:50:59 +1300

    Try to 'render' App with enzyme in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { configure, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+	render(<App />);
 });
